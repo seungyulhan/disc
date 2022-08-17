5730869092bc0a1fa33cc9c2a85cb24bbbd47b2b
Seungyul Han <49862563+seungyulhan@users.noreply.github.com>
2022-08-17 12:15:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ extras = {
     'test': [
         'filelock',
         'pytest',
-        'atari-py'
-    ],
-    'bullet': [
-        'pybullet',
+        'pytest-forked',
+        'atari-py',
+        'matplotlib',
+        'pandas'
     ],
     'mpi': [
         'mpi4py'
@@ -31,35 +31,30 @@ setup(name='baselines',
       packages=[package for package in find_packages()
                 if package.startswith('baselines')],
       install_requires=[
-          'tensorflow',
+          'gym>=0.15.4, <0.16.0',
           'scipy',
           'tqdm',
           'joblib',
-          'dill',
-          'progressbar2',
           'cloudpickle',
           'click',
-          'opencv-python',
-          'gym==0.7.4',
-          'mujoco-py==0.5.7',
-          'box2d-py',
+          'opencv-python'
       ],
       extras_require=extras,
       description='OpenAI baselines: high quality implementations of reinforcement learning algorithms',
       author='OpenAI',
       url='https://github.com/openai/baselines',
       author_email='gym@openai.com',
-      version='0.1.5')
+      version='0.1.6')
 
 
 # ensure there is some tensorflow build with version above 1.4
 import pkg_resources
 tf_pkg = None
-for tf_pkg_name in ['tensorflow', 'tensorflow-gpu']:
+for tf_pkg_name in ['tensorflow', 'tensorflow-gpu', 'tf-nightly', 'tf-nightly-gpu']:
     try:
         tf_pkg = pkg_resources.get_distribution(tf_pkg_name)
     except pkg_resources.DistributionNotFound:
         pass
 assert tf_pkg is not None, 'TensorFlow needed, of version above 1.4'
-from distutils.version import StrictVersion
-assert StrictVersion(re.sub(r'-?rc\d+$', '', tf_pkg.version)) >= StrictVersion('1.4.0')
+from distutils.version import LooseVersion
+assert LooseVersion(re.sub(r'-?rc\d+$', '', tf_pkg.version)) >= LooseVersion('1.4.0')
